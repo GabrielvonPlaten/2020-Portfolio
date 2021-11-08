71265d9b9f02eeba8b9561c307ba5c06c7749533
Gabriel von Platen <gabrielvonplaten@gmail.com>
2021-11-08 18:12:34 +0100

    Display recent github activity below work & projects section

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Home } from './Components/Home/Home';
 import { Work } from './Components/Work/Work';
 import { Footer } from './Components/Footer/Footer';
+import { Github } from './Components/Github/Github';
 
 const App: React.FC = () => {
   return (
     <div>
       <Home />
       <Work />
+      <Github />
+      {/* ------- */}
       <Footer />
     </div>
   );
